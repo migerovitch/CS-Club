d2224488667e8bd6a8de84d6c33568b118c4565b
Alex Giberson <alexgiberson7@gmail.com>
2019-10-08 12:03:26 -0400

    Updated pacman and level editor

--- AlexGiberson/pacman.py
+++ AlexGiberson/pacman.py
@@ -21,18 +21,20 @@ grey = (128,128,128)
 darkBlue = (0,20,121)
 
 #starting coords of player
-pacX = 0
-pacY = 0
+pacX = 40
+pacY = 40
 
 yChange = 0
 xChange = 0
 
-xy = []
-rx = []
-ry = []
-restrictedxy = []
+score = 0
 
-levelname = (input("Levelname (omit file tage):"))
+
+xy = []
+blocks = []
+barriers = []
+food = []
+levelname = (input("Levelname (omit file tag):"))
 
 #if true then no entry
 touching = False
@@ -43,11 +45,25 @@ gameExit = False
 csvfile =open(levelname +".csv",'r')
 obj=csv.reader(csvfile)
 for row in obj:
-	xy.append((row))
+	blocks.append((row))
 
-def buildlevel(xy,screen,color):
-	for coord in xy:
-		pygame.draw.rect(screen,color,[float(coord[0]),float(coord[1]),float(coord[2]),float(coord[3])])
+
+
+for block in blocks:
+	if block[0] == "barrier":
+		barriers.append(block)
+	elif block[0] == "food":
+		food.append(block)
+print(barriers)
+print(food)
+
+def buildlevel(xy,screen,bcolor,fcolor):
+	for coord in barriers:
+		pygame.draw.rect(screen,bcolor,[float(coord[1]),float(coord[2]),float(coord[3]),float(coord[4])])
+	for coord in food:
+		pygame.draw.rect(screen,fcolor,[float(coord[1]),float(coord[2]),float(coord[3]),float(coord[4])])
+
+maxscore = len(food)
 
 
 #main game loop
@@ -89,27 +105,42 @@ while gameExit == False:
 		if pacY < 0: pacY = height - playersize
 
 	#is the movement selected going to intersect with a boundary?
-	"""
-	if pacX in rx and pacY in ry:
-		touching = True
-	else:
-		touching = False
-	"""
-	
+	for coord in barriers:
+		x = float(coord[1])
+		y = float(coord[2])
+		rectwidth = float(coord[3])
+		rectheight = float(coord[4])
 
-	
-	#if not touching a boundary then allow move
-	
+		if pacX+xChange > round(x-playersize) and pacX+xChange < round(x+rectwidth) and pacY+yChange > round(y-playersize) and pacY+yChange < round(y+rectheight):
+			touching = True
+
+#if player touching barrier then dont allow move
 	if not touching:
 		pacX += xChange
 		pacY += yChange
-	elif touching == True:
-		print("Sorry, that's out of bounds...")
+	touching = False
+
+	for coord in food:
+		x = float(coord[1])
+		y = float(coord[2])
+		rectwidth = float(coord[3])
+		rectheight = float(coord[4])
+
+		if pacX+xChange > round(x-playersize) and pacX+xChange < round(x+rectwidth) and pacY+yChange > round(y-playersize) and pacY+yChange < round(y+rectheight):
+			score += 1 
+			food.remove(coord)
+			print(score)
+
+	if score == maxscore:
+		print("You Win!")
+		gameExit = True
+
+	
 	
 	#draw everything on the display
 	DISPLAY.fill(grey)
 	pygame.draw.rect(DISPLAY,yellow,[pacX,pacY,playersize,playersize])
-	buildlevel(xy,DISPLAY,darkBlue)
+	buildlevel(xy,DISPLAY,darkBlue,white)
 	pygame.display.update()
 	clock.tick(fps)
 
